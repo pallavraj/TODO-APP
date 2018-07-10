3c38a7e23c383dc2761f1b397d592b8b497bacf2
pallavraj <pallavraj40@gmail.com>
2018-07-10 19:25:39 +0530

    JS ADDED

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
- 
+
+var uList = document.getElementById('list');
+var li;
+
+
+
+var addButton = document.getElementById('add')
+
+addButton.addEventListener('click', addItem);
+
+var removeButton = document.getElementById('remove')
+
+removeButton.addEventListener('click',removeItem)
+
+var removeAll = document.getElementById('removeAll');
+
+removeAll.addEventListener('click',removeAllItem);
+
+function addItem() {
+
+
+
+}
+
+function removeItem() {
+li = uList.children
+    
+    for (let index = 0;index< li.length;index++){
+
+    while(li[index] && li[index].children[0].checked){
+        uList.removeChild(li[index])
+    }
+    }
+}
+
+function removeAllItem() {
+    li = uList.children
+
+    for (let index = 0;index< li.length;index++){
+
+        while(li[index] && li[index].children){
+            uList.removeChild(li[index])
+        }
+
+    }
+}
+
+
